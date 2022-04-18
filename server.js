7f26614cc4c0ed3da31b4eed38536a93bd390096
Glitch (glitch-blank-node) <none>
2022-04-18 13:22:15 +0000

    🌚🌮 Checkpoint ./server.js:577339/4340

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ mongo.connect();
 const db = mongo.db()
 
 // Discord
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES] });
 client.once('ready', () => {
 	console.log('Ready!');
 });
@@ -22,7 +22,7 @@ client.on('interactionCreate', async interaction => {
   console.log(interaction)
 	if (commandName === 'showdown') {
     const string = String(interaction.options.getString('command')).trim();
-    console.log(`[${new Date().toJSON()}] ${interaction.user.tag} => ${string}`)
+    console.log(`[${new Date().toJSON()}] ${interaction.user.tag} DM=> ${string}`)
     
     let m
     if (m = string.match(/^set\s+email\s+(\S+)$/)) {
@@ -36,6 +36,13 @@ client.on('interactionCreate', async interaction => {
 		  await interaction.reply('unrecognized command!');
     }
 	}
+});
+client.on('messageCreate', async message => {
+  if (message.mentions.has(client.user) && !message.author.bot) {
+    console.log(`[${new Date().toJSON()}] ${message.author.tag} Message=>`, message)
+    let isAdmin = message.author.id === '104986860236877824'
+    message.reply(`whee ${isAdmin}`)
+  }
 });
 
 // Require the fastify framework and instantiate it
