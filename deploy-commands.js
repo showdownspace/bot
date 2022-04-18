9f9b218de15e88ac33ce772b0c89a4c13c97f7df
Glitch (glitch-blank-node) <none>
2022-04-18 12:32:08 +0000

    🐩🐃 Checkpoint ./deploy-commands.js:577339/3 ./server.js:577339/36

--- deploy-commands.js
+++ deploy-commands.js
@@ -6,8 +6,7 @@ const commands = [
   new SlashCommandBuilder()
     .setName('showdown')
     .setDescription('Talk to showdown bot')
-	  .addStringOption(option => option.setName('command').setDescription('Command to set to showdown bot').setRequired(true))
-  ,
+	  .addStringOption(option => option.setName('command').setDescription('Command to set to showdown bot').setRequired(true)),
 ].map((command) => command.toJSON())
 
 const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
