fca286c53cbee66f437bd25bb6d1eca3664b5941
Glitch (glitch-blank-node) <none>
2022-04-19 14:45:45 +0000

    🐈⚾️ Checkpoint ./deploy-commands.js:577339/473

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,10 +3,10 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 
 const commands = [
-  new SlashCommandBuilder()
-    .setName('showdown')
-    .setDescription('Talk to showdown bot')
-	  .addStringOption(option => option.setName('command').setDescription('Command to set to showdown bot').setRequired(true)),
+  // new SlashCommandBuilder()
+  //   .setName('showdown')
+  //   .setDescription('Talk to showdown bot')
+  // .addStringOption(option => option.setName('command').setDescription('Command to set to showdown bot').setRequired(true)),
 ].map((command) => command.toJSON())
 
 const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
