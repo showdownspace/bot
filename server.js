331ca347e83ce59b58fc094c5d85c8de4bd5132c
Glitch (glitch-blank-node) <none>
2022-04-19 16:07:49 +0000

    🐕🚍 Checkpoint ./server.js:577339/9176

--- server.js
+++ server.js
@@ -1,83 +1,71 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents } = require("discord.js");
 const path = require("path");
 const util = require("util");
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const fs = require('fs')
-const crypto = require('crypto')
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const fs = require("fs");
+const crypto = require("crypto");
 
-let latestDeployment
-fs.mkdirSync('.data/blobs', { recursive: true })
-if (fs.existsSync('.data/latest_deployment')) {
-  latestDeployment = fs.readFileSync('.data/latest_deployment', 'utf8')
-  console.log('Latest deployment found.')
+let latestDeployment;
+fs.mkdirSync(".data/blobs", { recursive: true });
+if (fs.existsSync(".data/latest_deployment")) {
+  latestDeployment = fs.readFileSync(".data/latest_deployment", "utf8");
+  console.log("Latest deployment found.");
 } else {
-  console.log('No deployment found.')
+  console.log("No deployment found.");
 }
 
 async function loadLatestDeployment() {
-  const file = fs.realpathSync('.data/deployments/' + latestDeployment + '/index.js')
-  return require(file)
+  const file = fs.realpathSync(
+    ".data/deployments/" + latestDeployment + "/index.js"
+  );
+  return require(file);
 }
 
 // MongoDB
-const mongo = new MongoClient(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const mongo = new MongoClient(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 mongo.connect();
-const db = mongo.db()
+const db = mongo.db();
 
 // Discord
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES],
-  partials: ["CHANNEL"]
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.DIRECT_MESSAGES,
+  ],
+  partials: ["CHANNEL"],
 });
-client.once('ready', () => {
-	console.log('Ready!');
+client.once("ready", () => {
+  console.log("Ready!");
 });
 const discordToken = process.env.DISCORD_TOKEN;
 client.login(discordToken);
-const context = { client, db, discordToken }
+const context = { client, db, discordToken };
 
-client.on('interactionCreate', async interaction => {
-  const logic = await loadLatestDeployment()
-  return logic.handleInteraction(context, interaction)
-
-	if (!interaction.isCommand()) return;
-
-	const { commandName } = interaction;
-
-  console.log(interaction)
-	if (commandName === 'showdown') {
-    const string = String(interaction.options.getString('command')).trim();
-    console.log(`[${new Date().toJSON()}] ${interaction.user.tag} Slash=> ${string}`)
-    
-    let m
-    if (m = string.match(/^set\s+email\s+(\S+)$/)) {
-      await db.collection('profiles').updateOne(
-        { _id: `discord${interaction.user.id}` },
-        { $set: { discordUserId: interaction.user.id, discordTag: interaction.user.tag, proposedEmail: m[1] } },
-        { upsert: true }
-      )
-		  await interaction.reply('saved your email, thanks!');
-    } else {
-		  await interaction.reply('unrecognized command!');
-    }
-	}
+client.on("interactionCreate", async (interaction) => {
+  const logic = await loadLatestDeployment();
+  return logic.handleInteraction(context, interaction);
 });
-client.on('messageCreate', async message => {
-  const logic = await loadLatestDeployment()
-  return logic.handleMessage(context, message)
+client.on("messageCreate", async (message) => {
+  const logic = await loadLatestDeployment();
+  return logic.handleMessage(context, message);
 });
 
 // Require the fastify framework and instantiate it
 const fastify = require("fastify")({
   // set this to true for detailed logging:
   logger: true,
-  bodyLimit: 10 * 1048576
+  bodyLimit: 10 * 1048576,
 });
 
 // Setup our static files
 fastify.register(require("fastify-static"), {
   root: path.join(__dirname, "public"),
-  prefix: "/" // optional: default '/'
+  prefix: "/", // optional: default '/'
 });
 
 // fastify-formbody lets us parse incoming forms
@@ -86,69 +74,72 @@ fastify.register(require("fastify-formbody"));
 // point-of-view is a templating manager for fastify
 fastify.register(require("point-of-view"), {
   engine: {
-    handlebars: require("handlebars")
-  }
+    handlebars: require("handlebars"),
+  },
 });
 
 function raise(code, message) {
-  const err = new Error(message)
-  err.statusCode = code
-  throw err
+  const err = new Error(message);
+  err.statusCode = code;
+  throw err;
 }
 
-fastify.post('/deploy', async (request, reply) => {
+fastify.post("/deploy", async (request, reply) => {
   if (request.body.token !== process.env.CODE_DEPLOY_TOKEN) {
-    raise(401, 'Invalid deploy key')
+    raise(401, "Invalid deploy key");
   }
   for (const { filename, data, hash } of request.body.files) {
-    const blobPath = `.data/blobs/${hash}`
+    const blobPath = `.data/blobs/${hash}`;
     if (data == null && !fs.existsSync(blobPath)) {
-      raise(400, 'Missing data for hash ' + hash)
+      raise(400, "Missing data for hash " + hash);
     }
     if (data != null) {
-      fs.writeFileSync(blobPath, data, 'utf8')
+      fs.writeFileSync(blobPath, data, "utf8");
     }
   }
-  const hashes = request.body.files.map(f => f.hash).sort()
-  const deploymentHash = crypto.createHash('sha256').update(hashes.join(',')).digest('hex')
-  const deploymentPath = `.data/deployments/${deploymentHash}`
-  fs.mkdirSync(deploymentPath, { recursive: true })
+  const hashes = request.body.files.map((f) => f.hash).sort();
+  const deploymentHash = crypto
+    .createHash("sha256")
+    .update(hashes.join(","))
+    .digest("hex");
+  const deploymentPath = `.data/deployments/${deploymentHash}`;
+  fs.mkdirSync(deploymentPath, { recursive: true });
   for (const { filename, data, hash } of request.body.files) {
-    const blobPath = `.data/blobs/${hash}`
+    const blobPath = `.data/blobs/${hash}`;
     try {
-      fs.linkSync(blobPath, `${deploymentPath}/${filename}`)
+      fs.linkSync(blobPath, `${deploymentPath}/${filename}`);
     } catch (error) {
-      if (error.code !== 'EEXIST') {
-        throw error
+      if (error.code !== "EEXIST") {
+        throw error;
       }
     }
   }
-  fs.writeFileSync('.data/latest_deployment', deploymentHash)
-  latestDeployment = deploymentHash
-  return 'meow'
-})
+  fs.writeFileSync(".data/latest_deployment", deploymentHash);
+  latestDeployment = deploymentHash;
+  return "meow";
+});
 
 // Our main GET home page route, pulls from src/pages/index.hbs
-fastify.get("/", function(request, reply) {
+fastify.get("/", function (request, reply) {
   // params is an object we'll pass to our handlebars template
   let params = {
-    greeting: "Hello Node!"
+    greeting: "Hello Node!",
   };
   // request.query.paramName <-- a querystring example
   reply.view("/src/pages/index.hbs", params);
 });
 
 // A POST route to handle form submissions
-fastify.post("/", function(request, reply) {
+fastify.post("/", function (request, reply) {
   let params = {
-    greeting: "Hello Form!"
+    greeting: "Hello Form!",
   };
   // request.body.paramName <-- a form post example
   reply.view("/src/pages/index.hbs", params);
 });
 
 // Run the server and report out to the logs
-fastify.listen(process.env.PORT, '0.0.0.0', function(err, address) {
+fastify.listen(process.env.PORT, "0.0.0.0", function (err, address) {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
